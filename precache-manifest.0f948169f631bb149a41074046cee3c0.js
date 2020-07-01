27433b9046607874ea7acb53ace9420cb56a419e
pravindeva <30575339+pravindeva@users.noreply.github.com>
2020-07-01 20:01:49 +0530

    Updates

---
+++ precache-manifest.0f948169f631bb149a41074046cee3c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad2420ea2ee94228cdd55f557e323cae",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "612e5ee40a29a2d8b82a",
+    "url": "/tic-tac-toe/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7bcf1dd808c4fce10798",
+    "url": "/tic-tac-toe/static/css/main.fb732a88.chunk.css"
+  },
+  {
+    "revision": "612e5ee40a29a2d8b82a",
+    "url": "/tic-tac-toe/static/js/2.c7a907ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.c7a907ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bcf1dd808c4fce10798",
+    "url": "/tic-tac-toe/static/js/main.7ca4f8b6.chunk.js"
+  },
+  {
+    "revision": "ee9f5e129b0c825ad32f",
+    "url": "/tic-tac-toe/static/js/runtime-main.b8d42af8.js"
+  }
+]);
